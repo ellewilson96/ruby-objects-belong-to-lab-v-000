95e33cb860729f30823c96478365b2db0b19b20c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-01 17:56:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
-    attr_accessor :title, :artist, :name
-
-    def initialize(title)
-      @title = title
-    end
-
+ 
+  attr_accessor :title
+ 
+  def initialize(title)
+    @title = title
   end
+end
